10d2153913ee1bd3933d0dc1d234b1bc0dcae831
Elena Poganova <a.n.rus@mail.ru>
2023-01-27 10:48:24 +0400

    newWork1

---
+++ newWork1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()), count = a;
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < b; i++)
+{
+    count = count * a;
+}
+Console.WriteLine(count);
