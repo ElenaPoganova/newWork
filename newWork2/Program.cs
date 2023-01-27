f47192e2314b76d75d3ac36451dfc2da53fc9530
Elena Poganova <a.n.rus@mail.ru>
2023-01-27 10:49:26 +0400

    newWork2

---
+++ newWork2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Console.Clear();
+Console.Write("Введите число: ");
+string?stroka = Console.ReadLine();
+char n = '0';
+char[]array = new char[n]; 
+int sum = 0, count = 1;
+for(int i=0; i <= array.Length; i++)
+{
+    array[i] = Convert.ToInt32(stroka[i].ToString());
+    count = count + i;
+    sum = sum + n;
+}
+
+Console.WriteLine(sum);*/
