3f451c4a4b2e9726571b9bbcb2cb4ae3e97c8887
Elena Poganova <a.n.rus@mail.ru>
2023-01-31 10:55:56 +0400

    Sem1

---
+++ Sem1/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10); 
+}
+
+int summ = 0;
+void ReleaseArray(int[] array)
+ {
+    for (int i = 1; i < array.Length; i+=2)
+    summ +=array[i];
+    Console.Write($"{summ}");
+ }
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+
