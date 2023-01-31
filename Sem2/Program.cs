162143889432a11ba655ec803e8791976233fc12
Elena Poganova <a.n.rus@mail.ru>
2023-01-31 10:56:45 +0400

    Sem2

---
+++ Sem2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array [i] = Convert.ToDouble(Console.ReadLine());
+}
+
+void ReleaseArray(double[]array)
+{
+    double Result = 0, max = 0, min = 0;
+    double Result = max - min;
+    {
+        for (int i = 0; i < array.Length; i++)
+            a[i] > max, a[i] = max;
+            a[i] < min, a[i] = min;
+    }
+
+}
+
+Console.Clear();
+Console.Write("Введите элементы массива: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[5];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array, Result);*/
+
+
+  
+
